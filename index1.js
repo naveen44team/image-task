4cd974d550fb0a820ff53e6271428d006be374c2
naveen44team <96235742+naveen44team@users.noreply.github.com>
2022-01-13 18:54:14 +0530

    image task completed

---
+++ index1.js
@@ -0,0 +1,25 @@
+const url1 = "https://wallpapercave.com/wp/wp7058717.jpg";
+const img1 = document.createElement("img");
+img1.setAttribute("src",url1);
+img1.setAttribute("class","img-all");
+document.body.append(img1);
+
+const url2 = "https://wallpaperaccess.com/full/773271.jpg";
+const img2 = document.createElement("img");
+img2.setAttribute("src", url2);
+img2.setAttribute("class","img-all");
+document.body.append(img2);
+
+const url3 = "https://3.bp.blogspot.com/-QYDG3HoV0cY/WFpbEXRwDQI/AAAAAAAACkE/qpLpOrt1L3oiKZLS-Lm5TtOkTkiYMnYYQCLcB/s1600/Maldives%2BBeach%2BScenery%2BHD%2BWallpaper.jpg";
+const img3 = document.createElement("img");
+img3.setAttribute("src", url3);
+img3.setAttribute("class", "img-all");
+document.body.append(img3);
+
+const url4 = "https://mocah.org/uploads/posts/343911-Neon-Scenery-Landscape-Digital-Art.jpg";
+const img4 = document.createElement("img");
+img4.setAttribute("src", url4);
+img4.setAttribute("class","img-all");
+document.body.append(img4);
+
+const allurl = [url1, url2, url3,url4];
